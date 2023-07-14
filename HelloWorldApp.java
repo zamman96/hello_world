302930e7df9a17c4596eb20bd9948c3cd94533ce
user <user@192.168.219.101>
2023-07-14 15:56:06 +0900

    initial commit

---
+++ HelloWorldApp.java
@@ -0,0 +1,7 @@
+public class HelloWorldApp { 
+			//파일 이름과 똑같아야함
+	public static void main(String[] args) {
+		System.out.println("Hello World!!"); //문장을 마칠 땐 꼭 ;(세미클론)을 넣을 것
+	}
+}
+//저장하면 class라는 파일을 컴파일 시켜줌
